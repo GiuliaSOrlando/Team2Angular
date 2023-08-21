c52e6cc208b46721e917e7704ae2593ead8cd2cf
ElisaGelmetti <134309337+ElisaGelmetti@users.noreply.github.com>
2023-08-21 17:43:09 +0200

    elisa

--- LinkedIn/src/environments/environment.development.ts
+++ LinkedIn/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGUzMGQwYzFmMTc1YzAwMTRjNTU4YjYiLCJpYXQiOjE2OTI2MDE2MTMsImV4cCI6MTY5MzgxMTIxM30.xYioVY2wn6gyTK1ovP9nn8HBPDPC7tltj5oEMDCnqjY',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGUzMTE5YTFmMTc1YzAwMTRjNTU4YmEiLCJpYXQiOjE2OTI2MDI5MTcsImV4cCI6MTY5MzgxMjUxN30.CFGr8r89ECwsEwOntXqY7exfFWRiVBEZy-rIndgAaFI',
 } as { production: boolean; apiKey: string };
