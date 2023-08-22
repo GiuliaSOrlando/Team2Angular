46e336872c25a0c9fecabc4ddbe385075bdf84a7
GiuliaSOrlando <134381769+GiuliaSOrlando@users.noreply.github.com>
2023-08-22 18:19:26 +0200

    Navbar error fix

--- LinkedIn/src/app/users.service.ts
+++ LinkedIn/src/app/users.service.ts
@@ -44,21 +44,6 @@ export class UsersService {
     return this.http.put<IUser>(apiUrl, data);
   }
 
-  // Get experience
-  getExperience(userId: string): Observable<IExperience> {
-    const apiUrl = `https://striveschool-api.herokuapp.com/api/profile/${userId}/experiences`;
-    return this.http.get<IExperience>(apiUrl);
-  }
-
-  // Create experience
-  createExperience(
-    userId: string,
-    data: Partial<IExperience>
-  ): Observable<IExperience> {
-    const apiUrl = `https://striveschool-api.herokuapp.com/api/profile/${userId}/experiences`;
-    return this.http.post<IExperience>(apiUrl, data);
-  }
-
   // updateUser(data: Partial<IUser>) {
   //   const apiUrl = 'https://striveschool-api.herokuapp.com/api/profile/';
   //   return this.http.put<IUser>(apiUrl, data).pipe(
