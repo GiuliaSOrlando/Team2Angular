c3a5abf54256c2684b2de8bed3588e572eaaec7e
Claudio Galiano <claudiogal92@gmail.com>
2023-08-22 12:24:20 +0200

    modale experience

--- LinkedIn/src/app/Components/experience/experience.component.ts
+++ LinkedIn/src/app/Components/experience/experience.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.scss']
+  styleUrls: ['./experience.component.scss'],
 })
 export class ExperienceComponent {
+  @ViewChild('content') content!: any;
 
+  isExperiencePage: boolean = false;
+
+  constructor(
+    private modalService: NgbModal,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.route.url.subscribe((urlSegments) => {
+      this.isExperiencePage = urlSegments[0]?.path === 'experience';
+    });
+  }
+
+  // MODALE
+  open(content: any) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' });
+  }
+
+  // Funzione sezione esperienze
+  buttonHandler() {
+    if (this.isExperiencePage) {
+      open(this.content);
+    } else {
+      this.router.navigate(['/experience']);
+    }
+  }
 }
