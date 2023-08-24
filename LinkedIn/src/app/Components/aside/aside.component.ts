541ecf65e27de77178bc1dee2286f96e5b8ccbe7
GiuliaSOrlando <134381769+GiuliaSOrlando@users.noreply.github.com>
2023-08-24 08:57:40 +0200

    Minor edit on the aside

--- LinkedIn/src/app/Components/aside/aside.component.ts
+++ LinkedIn/src/app/Components/aside/aside.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { UsersService } from 'src/app/users.service';
+import { IUser } from '../Interfaces/user';
 
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
-  styleUrls: ['./aside.component.scss']
+  styleUrls: ['./aside.component.scss'],
 })
 export class AsideComponent {
+  constructor(private userSVC: UsersService) {}
+  user!: IUser;
+  username!: string;
+  _id!: string;
 
+  ngOnInit() {
+    this.getMyProfile();
+  }
+
+  getMyProfile() {
+    this.userSVC.getSingleUser().subscribe((user: IUser) => {
+      this.user = user;
+      this.username = user.username;
+    });
+  }
 }
